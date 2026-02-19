33dc673e465b0e852cb2a405d202f7c9cd54eafc
Richard Patel <ripatel@jumptrading.com>
2026-02-19 08:17:52 +0000

    no setjmp

--- src/fdos/kern/fdos_kern.c
+++ src/fdos/kern/fdos_kern.c
@@ -19,9 +19,13 @@ __attribute__((naked)) void
 syscall_handler( void ) {
   __asm__ volatile (
       "cmp $231, %eax;\n"
-      "je longjmp;\n"
-      "sub $128, %rsp;\n"
+      "jne .slow_path;\n"
+      "movq $0xffffff80003fe008, %rbp;\n"
+      "movq %rbp, %rsp;\n"
+      "jmp ring0_entrypoint;\n"
       /* This is probably a bit overkill */
+      ".slow_path:"
+      "sub $128, %rsp;\n"
       "pushq %rbp;\n"
       "pushq %rdi;\n"
       "pushq %rsi;\n"
@@ -79,66 +83,10 @@ fd_io_write( int          fd,
 
 /* Context switching **************************************************/
 
-__attribute__((aligned(64))) ulong g_save[ 4096 ];
+static fdos_kern_args_t * g_args;
 
-// __attribute__((naked)) uint
-// setjmp_xsave( void ) {
-//   __asm__ volatile (
-//       "movabsq $g_save, %rdi;\n"
-//       "leaq 8(%rsp), %rdx;\n"
-//       "movq %rdx, 0(%rdi);\n"   /* g_save[0] -> stack pointer after return */
-//       "movq (%rsp), %rdx;\n"
-//       "movq %rdx, 8(%rdi);\n"   /* g_save[1] -> return address */
-//       "xor %eax, %eax;\n"
-//       "xor %edx, %edx;\n"
-//       "xsaveoptq 64(%rdi);\n"
-//       "retq;\n"
-//   );
-// }
-
-// __attribute__((naked,noreturn)) void
-// longjmp_xsave( void ) {
-//   __asm__ volatile (
-//       "movabsq $g_save, %rdi;\n"
-//       "xrstorq 64(%rdi);\n"
-//       "movq 0(%rdi), %rsp;\n"  /* restore stack pointer  */
-//       "jmp *8(%rdi);\n"        /* jump to return address */
-//   );
-// }
-
-__attribute__((naked)) uint
-setjmp( void ) {
-  __asm__ volatile (
-      "movabsq $g_save, %rsi;\n"
-      "movq %rbx, (%rsi);\n"
-      "movq %rbp, 8(%rsi);\n"
-      "movq %r12, 16(%rsi);\n"
-      "movq %r13, 24(%rsi);\n"
-      "movq %r14, 32(%rsi);\n"
-      "movq %r15, 40(%rsi);\n"
-      "leaq 8(%rsp), %rdx;\n"
-      "movq %rdx, 48(%rsi);\n"
-      "movq (%rsp), %rdx;\n"
-      "movq %rdx, 56(%rsi);\n"
-      "xorl %eax, %eax;\n"
-      "retq;\n"
-  );
-}
-
-__attribute__((naked,noreturn)) void
-longjmp( void ) {
-  __asm__ volatile (
-      "movabsq $g_save, %rdi;\n"
-      "movq (%rdi), %rbx;\n"
-      "movq 8(%rdi), %rbp;\n"
-      "movq 16(%rdi), %r12;\n"
-      "movq 24(%rdi), %r13;\n"
-      "movq 32(%rdi), %r14;\n"
-      "movq 40(%rdi), %r15;\n"
-      "movq 48(%rdi), %rsp;\n"
-      "jmp *56(%rdi);\n"
-  );
-}
+__attribute__((noreturn)) void
+ring0_entrypoint( void );
 
 ulong
 syscall_handler1( ulong arg0,
@@ -147,24 +95,23 @@ syscall_handler1( ulong arg0,
                   uint  num ) {
   (void)arg2;
   switch( num ) {
-  case 231: /* exit_group */
-    longjmp();
   case 257: /* openat */
     /* FIXME validate arg1 pointer */
     FD_LOG_CRIT(( "rejected userland syscall 'openat(%d,%s)', aborting", (int)arg0, (char const *)arg1 ));
   default:
+    __asm__ ("hlt" );
     FD_LOG_CRIT(( "unsupported syscall %u", num ));
   }
 }
 
 /* ring3 API */
 
-__attribute__((naked)) void
-ring3_enter( ulong user_stack_top_gpaddr, /* rdi */
-             ulong function ) {           /* rsi */
+__attribute__((naked,noreturn)) void
+ring3_enter( ulong stack,       /* rdi */
+             ulong function ) { /* rsi */
   __asm__ volatile (
       "pushq $0x23;\n" /* segment 4 */
-      "pushq %rdi;\n"  /* user stack */
+      "pushq %rdi;\n"
       "pushq $0x1b;\n" /* segment 3 */
       "pushq %rsi;\n"
       "lretq;\n"
@@ -180,41 +127,37 @@ ring3_end( void ) {
   );
 }
 
-static void
-ring3_run( ulong stack_top_gvaddr,
-           ulong func ) {
-  if( setjmp()==0 ) {
-    ring3_enter( stack_top_gvaddr, func );
-    __builtin_unreachable();
+__attribute__((noreturn)) void
+ring0_entrypoint( void ) {
+  static ulong iter = 0UL;
+  static long start;
+  if( FD_UNLIKELY( !iter ) ) {
+    FD_LOG_NOTICE(( "Benchmarking" ));
+    start = fd_log_wallclock();
   }
+  else if( FD_UNLIKELY( iter>(ulong)1e7 ) ) {
+    FD_LOG_ERR(( "Done (%lu ns/iter)", (ulong)( (double)(fd_log_wallclock()-start)/(double)iter ) ));
+  }
+  iter++;
+  fdos_kern_args_t * args = g_args;
+  ulong const ustack = args->stack_user_top_gvaddr-8UL;
+  ulong const uentry = args->ring3_entry_gvaddr;
+  ring3_enter( ustack, uentry );
 }
 
 __attribute__((noreturn)) void
 fdos_kern_main( fdos_kern_args_t * args ) {
+  g_args = args;
   g_pvclock = (fd_pvclock_t *)args->pvclock_gvaddr;
 
   fd_log_thread_set( "kvm0" );
   fd_log_wallclock_set( fd_pvclock_now, g_pvclock );
   fd_log_colorize_set( 1 );
 
-  FD_LOG_NOTICE(( "Hello world!" ));
   ulong const ustack = args->stack_user_top_gvaddr-8UL;
   ulong const uentry = args->ring3_entry_gvaddr;
   FD_STORE( ulong, (void *)ustack, (ulong)ring3_end );
-
-  ring3_run( ustack, uentry );
-  FD_LOG_NOTICE(( "Returned from ring 3" ));
-
-  FD_LOG_NOTICE(( "Benchmarking" ));
-  long dt = -fd_log_wallclock();
-  ulong iter = (ulong)1e7;
-  for( ulong i=0UL; i<iter; i++ ) {
-    ring3_run( ustack, uentry );
-  }
-  dt += fd_log_wallclock(); (void)dt;
-  FD_LOG_NOTICE(( "Context switching: %lu ns/iter", (ulong)( (double)dt/(double)iter ) ));
-
-  FD_LOG_ERR(( "Goodbye" ));
+  ring3_enter( ustack, uentry );
 }
 
 __attribute__((noreturn)) void
